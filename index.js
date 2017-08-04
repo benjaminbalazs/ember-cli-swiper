e4c3796d3b3e34481a7ca8ce5fcf8bbb77d00814
benjaminbalazs@gmail.com <benjaminbalazs@Benjamins-iMac-2.local>
2017-08-04 19:12:47 +0400

    fastboot

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 /* jshint node:true */
 'use strict';
-var map = require('broccoli-stew').map;
+
 var path = require('path');
 var Funnel = require('broccoli-funnel');
-var mergeTrees = require('broccoli-merge-trees');
+var MergeTrees = require('broccoli-merge-trees');
+var FastbootTransform = require('fastboot-transform');
 
 module.exports = {
-  name: 'ember-cli-swiper',
 
-  treeForVendor: function(defaultTree) {
-    var swiperPath = path.join(path.dirname(require.resolve('swiper')), '..');
-    var browserVendorLib = new Funnel(swiperPath, {
-      destDir: 'swiper',
-      include: ['js/swiper.min.js', 'css/swiper.min.css']
-    });
+    name: 'ember-cli-swiper',
 
-    browserVendorLib = map(browserVendorLib, (content, relativePath) => {
-      if (relativePath.indexOf('css') !== -1) {
-        return content;
-      }
-      return `if (typeof FastBoot === 'undefined') { ${content} }`
-    });
-  
-    return new mergeTrees([defaultTree, browserVendorLib]);
-  },
+    included: function(app) {
 
-  included: function(app) {
-    this._super.included.apply(this, arguments);
+        this._super.included.apply(this, arguments);
 
-    this.app.import('vendor/swiper/js/swiper.min.js');
-    this.app.import('vendor/swiper/css/swiper.min.css');
-  }
+        app.import(this.treePaths.vendor + '/swiper/js/swiper.min.js');
+        app.import(this.treePaths.vendor + '/swiper/css/swiper.min.css');
+
+    },
+
+    treeForVendor: function(vendorTree) {
+
+        var trees = [];
+        if ( vendorTree !== undefined ) { trees.push(vendorTree); }
+
+        var swiperTree = FastbootTransform(new Funnel(path.join(path.dirname(require.resolve('swiper')), '..'), {
+            destDir: 'swiper',
+            include: ['js/swiper.min.js', 'css/swiper.min.css']
+        }));
+
+        trees.push(swiperTree);
+
+        //
+
+        return new MergeTrees(trees);
+
+    },
+
+    isDevelopingAddon: function() {
+        return true;
+    }
 
 };
